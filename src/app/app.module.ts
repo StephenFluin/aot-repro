50adcb475234ac19805854e4dc911f2c04ba5d2c
Stephen Fluin <stephen.fluin@gmail.com>
2016-12-21 09:56:33 -0800

    Add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +12,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAJawulOMYRp0eXjMHLqiffzuS9tToCfAI",
+      authDomain: "fluindotio-website-93127.firebaseapp.com",
+      databaseURL: "https://fluindotio-website-93127.firebaseio.com",
+      storageBucket: "fluindotio-website-93127.appspot.com",
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
